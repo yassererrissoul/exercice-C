38eb2bcf5f896ec577aa07b0c8536560df62dec2
YASSER <139642962+yassererrissoul@users.noreply.github.com>
2023-12-09 15:37:28 +0100

    Create ft_lower_case.c

---
+++ ft_lower_case.c
@@ -0,0 +1,13 @@
+char *ft_lower_case(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i]=str[i]+32;
+        }
+        i++;
+    }
+    return (str);
+}
