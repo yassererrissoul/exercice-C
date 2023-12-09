42c94f766d3df430cfc3e18a8d35478690b49889
YASSER <139642962+yassererrissoul@users.noreply.github.com>
2023-12-09 15:34:31 +0100

    Create ft_upper_case.c

---
+++ ft_upper_case.c
@@ -0,0 +1,13 @@
+char *ft_upper_case(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i]=str[i]-32;
+        }
+        i++;
+    }
+    return (str);
+}
